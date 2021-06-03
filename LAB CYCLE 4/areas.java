479e9b7ade737537440d29768b62ca2c90f8b2dc
nourin <65014502+fathimanovrin@users.noreply.github.com>
2021-06-03 21:14:32 +0530

    q1

---
+++ LAB CYCLE 4/areas.java
@@ -0,0 +1,18 @@
+package graphics;
+import graphics.shapes;
+
+public class areas{
+public static void main(String args[])
+{
+    Shapes obj=new Shapes();
+    System.out.println("Area of different Shapes"+"\n"+"-------------------------------");
+    System.out.println("Circle"+"\n"+"-------------");
+    obj.carea();
+    System.out.println("Rectangle"+"\n"+"-------------");
+    obj.rarea();
+    System.out.println("Triangle"+"\n"+"--------------");
+    obj.tarea();
+    System.out.println("Square"+"\n"+"------------");
+    obj.sarea();
+}
+}
