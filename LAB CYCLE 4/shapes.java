f9d335d6458bea6f8abc609baedad85214691d16
nourin <65014502+fathimanovrin@users.noreply.github.com>
2021-06-03 21:12:37 +0530

    1/graphics/shapes

---
+++ LAB CYCLE 4/shapes.java
@@ -0,0 +1,45 @@
+package graphics;
+import java.util.Scanner;
+interface calculate
+{
+    void carea();
+    void rarea();
+    void tarea();
+    void sarea();
+
+}
+public class shapes implements calculate
+{
+    Scanner sc=new Scanner(System.in);
+    public void carea()
+    {
+        System.out.println("Enter radius:");
+        int r=sc.nextInt();
+        System.out.println("Area of circle:"+(Math.PI*r*r));
+    }
+    public void rarea()
+    {
+        System.out.println("Enter length:");
+        int l=sc.nextInt();
+        System.out.println("Enter breadth:");
+        int b=sc.nextInt();
+        System.out.println("Area of Rectangle:"+(l*b));
+    }
+    public void tarea()
+    {
+        System.out.println("Enter the base:");
+		int b = sc.nextInt();
+		
+		System.out.println("Enter the height:");
+		int h = sc.nextInt();
+		
+		System.out.println("Area of the triangle:"+(0.5*b*h));
+    }
+    public void sarea()
+    {
+        System.out.println("Enter the side:");
+		int s = sc.nextInt();
+		
+		System.out.println("Area of the square:"+(s*s));
+    }
+}
