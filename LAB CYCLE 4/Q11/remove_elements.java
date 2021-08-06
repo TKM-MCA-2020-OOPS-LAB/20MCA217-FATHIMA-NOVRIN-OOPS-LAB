4d4ada27eed86bf7d43f0e0d1a55fe46a3111d10
nourin <65014502+fathimanovrin@users.noreply.github.com>
2021-08-06 19:57:30 +0530

    linked list remove elemnts

---
+++ LAB CYCLE 4/Q11/remove_elements.java
@@ -0,0 +1,34 @@
+package myproject;
+import java.util.*;
+
+public class remove_elements {
+	public static void main(String args[]) {
+		LinkedList<String> list=new LinkedList<String>();
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter the number of items to be added");
+		 int num=sc.nextInt();
+		 System.out.print("Add items ");
+	      for(int i=0;i<num;i++)
+	       {
+	        	
+	            String s=sc.next();
+	            list.add(s);
+	        }
+	        System.out.print(" ");
+	        System.out.print(" ");
+	        System.out.println("liked list after adding");
+	       
+	        Iterator<String> itr=list.iterator();
+	        while(itr.hasNext()){
+	            System.out.println(itr.next());
+	        }
+	        list.clear();
+	        System.out.println("Linked list After removing: " + list);
+
+	    }
+		
+		
+		
+	}
+
+
